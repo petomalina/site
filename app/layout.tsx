84907e2fb63e8fb6b368d6609aa0864456bf3df1
Peter Malina <peter.malina@kiwi.com>
2023-07-24 12:16:10 +0200

    feat(analytics): add gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import HeaderMenu from './HeaderMenu'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-PL039FBCJB" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-PL039FBCJB');
+        `}
+      </Script>
+
       <body className={inter.className}>
         <HeaderMenu></HeaderMenu>
         {children}
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,10 +1,9 @@
 import fs from "fs";
 import matter from "gray-matter";
+import { MDXRemote } from 'next-mdx-remote/rsc'
 
 function getPost(slug: string) {
   const { data, content } = matter(fs.readFileSync(`posts/${slug}.md`, "utf8"));
-
-  console.log(data, content);
 
   return {
     data,
@@ -12,22 +11,26 @@ function getPost(slug: string) {
   }
 }
 
-export default function BlogPost(props: { params: { slug: string } }) {
+export default async function BlogPost(props: { params: { slug: string } }) {
   const post = getPost(props.params.slug);
 
   return (
-    <article>
-      <div className="relative aspect-[16/9] sm:aspect-[2/1]">
+    <article className="pt-12">
+      <div className="relative sm:aspect-[2.5/1] md:aspect-[3/1]">
         <img
           src={post.data.coverImage}
           alt=""
-          className="absolute inset-0 h-full w-full bg-gray-50 object-cover"
+          className="md:max-w-2xl lg:max-w-4xl mx-auto inset-0 h-full w-full bg-gray-50 object-cover"
         />
       </div>
-      <div className="py-24">
+      <div className="py-12">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:max-w-4xl">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{post.data.title}</h2>
+
+            <div className="text-gray-700 flex flex-col gap-4">
+              {await <MDXRemote source={post.content} />}
+            </div>
           </div>
         </div>
       </div>
