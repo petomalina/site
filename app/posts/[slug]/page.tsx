f01e79fb539cc21e5ac5457ae395baf34bc57cef
Peter Malina <peter.malina@kiwi.com>
2023-08-14 21:09:21 +0200

    fix: blog posts light theme

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -56,7 +56,7 @@ export default async function BlogPost(props: { params: { slug: string } }) {
         <div className="pb-6 pt-6 flex flex-row items-center">
           <button
             type="button"
-            className="rounded-full bg-indigo-600 p-1.5 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-full text-white bg-indigo-600 p-1.5 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             <ArrowLongLeftIcon className="h-5 w-5" aria-hidden="true" />
           </button>
@@ -83,10 +83,10 @@ export default async function BlogPost(props: { params: { slug: string } }) {
         </article>
       </main>
 
-      <div className="border-t-2 border-white p-4">
+      <div className="border-t-2 border-gray-800 dark:border-white p-4">
         <a href="https://www.linkedin.com/in/petomalina/" target="_blank" className="flex flex-row gap-4 justify-center items-center">
           <h3 className="font-bold">Struggling with delivery? Contact me on LinkedIn</h3>
-          <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" className="fill-white">
+          <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" className="fill-gray-800 dark:fill-white">
             <path d="M14.4 0H1.6C.7 0 0 .7 0 1.6v12.8C0 15.3.7 16 1.6 16h12.8c.9 0 1.6-.7 1.6-1.6V1.6c0-.9-.7-1.6-1.6-1.6zM4.8 13.2H2.4V6.4h2.4v6.8zm-1.2-8c-.8 0-1.2-.5-1.2-1.2 0-.7.5-1.2 1.2-1.2s1.2.5 1.2 1.2c0 .7-.5 1.2-1.2 1.2zm9.6 8h-2.4v-4.4c0-1.1-.4-1.8-1.3-1.8-.7 0-1.1.5-1.3 1-.1.2-.1.5-.1.8v4.4H6V6.4h2.3v1.1c.3-.5.8-1.2 2-1.2 1.5 0 2.7 1 2.7 3.2v4.7z"></path>
           </svg>
         </a>
